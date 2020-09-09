0d8e5bebe4b57f0aaee1283dad09370b8d22be99
Shivani_Bollabattin <shivani.bollabattin21@gmail.com>
2020-09-09 21:28:50 +0530

    initial commit

---
+++ CaseStudyRough/Reciever/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reciever
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Process compiler = new Process())
+            {
+                compiler.StartInfo.FileName = @"C:\Users\HP\source\repos\CaseStudyRough\Sender\bin\Debug\Sender.exe";
+                compiler.StartInfo.Arguments = "/r:System.dll /out:sample.exe stdstr.cs";
+                compiler.StartInfo.UseShellExecute = false;
+                compiler.StartInfo.RedirectStandardOutput = true;
+                compiler.Start();
+
+                Console.WriteLine(compiler.StandardOutput.ReadToEnd());
+
+                compiler.WaitForExit();
+            }
+
+
+
+            
+            
+        }
+    }
+}
---
+++ CaseStudyRough/Sender/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (var reader = new StreamReader(@"C:\Users\HP\source\repos\Sales.csv"))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
